c2072d5ae4df1630cc025092cf3c1e0cb570a977
StuHung <s3926369@rmit.edu.vn>
2022-08-26 00:32:30 +0700

    footer

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if (isset($_SESSION['role']) and !isset($_GET['logout'])) {
   <title>Homepage</title>
   <!-- CSS only -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous" />
-
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
   <link rel="stylesheet" href="./styles.css">
 </head>
 
