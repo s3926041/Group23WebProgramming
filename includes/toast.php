1e744ccf90bd77df5a6a2ebc032672003d6e9ee3
s3926041 <s3926041@rmit.edu.vn>
2022-08-26 12:25:56 +0700

    add

---
+++ includes/toast.php
@@ -0,0 +1,14 @@
+<div class='position-fixed toast-container top-0 start-50  p-4'>
+        <div class='toast border-0 translate-middle-x ' id='toastAlert'>
+            <div class='d-flex'>
+                <div class='toast-body'>
+                    <strong id='toast-text'></strong>
+                </div>
+                <button
+                    type='button'
+                    class='btn-close btn-close-white me-2 m-auto'
+                    data-bs-dismiss='toast'
+                ></button>
+            </div>
+        </div>
+    </div>
